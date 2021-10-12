3f2150a0523ed48de3c61593121f6255ea8c00ad
sachin kumar <merugune.sachin123@gmail.com>
2021-10-12 23:26:06 +0530

    comparision, logical and ternary operations are performed using javascript

---
+++ script.js
@@ -0,0 +1,60 @@
+// comparision operators
+
+if (10 == '10')
+  console.log(true)
+else
+  console.log(false)
+
+if (10 === '10')
+  console.log(true)
+else
+  console.log(false)
+
+if (10 != '10')
+  console.log(true)
+else
+  console.log(false)
+
+if (10 !== '10')
+  console.log(true)
+else
+  console.log(false)
+
+if (23 > 15)
+  console.log(true)
+else
+  console.log(false)
+
+if (23 >= 23)
+  console.log(true)
+else
+  console.log(false)
+
+if (23 < 15)
+  console.log(true)
+else
+  console.log(false)
+
+if (23 <= 23)
+  console.log(true)
+else
+  console.log(false)
+
+// logical operators
+if(8>6 && 12<20 && 5>=5)
+  console.log(true)
+else 
+  console.log(false)
+
+if(8>6 || 12<20 || 5>=5)
+  console.log(true)
+else 
+  console.log(false)
+
+var num1 = 22
+console.log(!(num1==22))
+
+// ternary operator
+
+ !(22>= 20) ? console.log(true) : console.log(false)
+
